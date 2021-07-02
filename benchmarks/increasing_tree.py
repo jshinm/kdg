d3ae15118f4883de8f8794ae7b5bb1db2883a885
jdey4 <jdey4@jhmi.edu>
2021-07-02 11:09:51 -0400

    plot edit

--- benchmarks/increasing_tree.py
+++ benchmarks/increasing_tree.py
@@ -15,9 +15,13 @@ n_test = 1000
 rf_med = []
 rf_25_quantile = []
 rf_75_quantile = []
+rf_selected_med = []
+rf_25_selected_quantile = []
+rf_75_selected_quantile = []
 # %%
 for n_estimator in n_estimators:
     err = []
+    err_ = []
     for _ in range(reps):
         X, y = sparse_parity(n_train)
         X_test, y_test = sparse_parity(n_test)
@@ -29,6 +33,13 @@ for n_estimator in n_estimators:
             )
         )
     
+        rf_model = rf(n_estimators=n_estimator).fit(X[:,:3], y)
+        err_.append(
+            1 - np.mean(
+                rf_model.predict(X_test[:,:3])==y_test
+            )
+        )
+
     rf_med.append(np.median(err))
     rf_25_quantile.append(
             np.quantile(err,[.25])[0]
@@ -36,15 +47,32 @@ for n_estimator in n_estimators:
     rf_75_quantile.append(
             np.quantile(err,[.75])[0]
         )
+
+    rf_selected_med.append(np.median(err_))
+    rf_25_selected_quantile.append(
+            np.quantile(err_,[.25])[0]
+        )
+    rf_75_selected_quantile.append(
+            np.quantile(err_,[.75])[0]
+        )
 #%%
 sns.set_context('talk')
 fig, ax = plt.subplots(1,1, figsize=(8,8))
 
 ax.plot(n_estimators, rf_med, c="k", label='RF')
 ax.fill_between(n_estimators, rf_25_quantile, rf_75_quantile, facecolor='k', alpha=.3)
+
+ax.plot(n_estimators, rf_selected_med, c="r", label='RF (feature selected)')
+ax.fill_between(n_estimators, rf_25_selected_quantile, rf_75_selected_quantile, facecolor='r', alpha=.3)
+
 ax.set_xticks(range(1,50,4))
+ax.set_xlabel('Tree #')
+ax.set_ylabel('Error')
+
 right_side = ax.spines["right"]
 right_side.set_visible(False)
 top_side = ax.spines["top"]
 top_side.set_visible(False)
+
+plt.savefig('plots/rf_increasing_tree.pdf')
 # %%
