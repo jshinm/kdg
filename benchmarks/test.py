50f3fe847f726248bb8c18c5fb87fbd774e45e87
jdey4 <jdey4@jhmi.edu>
2021-09-29 16:14:40 -0500

    new exp code

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -11,6 +11,8 @@ from sklearn.ensemble import RandomForestClassifier as rf
 from sklearn.metrics import cohen_kappa_score
 import os
 import math
+import matplotlib.pyplot as plt 
+import seaborn as sns
 # %%
 mc_reps = 10
 n_estimators = 500
@@ -21,7 +23,7 @@ total_sample = X.shape[0]
 unique_classes, counts = np.unique(y, return_counts=True)
 
 test_sample = 100
-train_samples = [10, 50, 100]
+train_samples = np.array([10, 50, 100])
 indx = []
 
 for label in unique_classes:
@@ -61,12 +63,12 @@ for train_sample in train_samples:
         model_kdf = kdf(feature_sub=2, kwargs={'n_estimators':500})
         model_kdf.fit(X[indx_to_take_train], y[indx_to_take_train])
         err.append(
-            np.mean(
+           1 - np.mean(
                 model_kdf.predict(X[indx_to_take_test])==y[indx_to_take_test]
             )
         )
         err_.append(
-            np.mean(
+            1 - np.mean(
                 model_kdf.rf_model.predict(X[indx_to_take_test])==y[indx_to_take_test]
             )
         )
@@ -90,4 +92,27 @@ for train_sample in train_samples:
     err_res_75_quantile_rf.append(
         np.quantile(err_,[.75])[0]
     )
+# %%
+sns.set_context('talk')
+fig, ax = plt.subplots(1,1, figsize=(8,8))
+
+ax.plot(train_samples*len(unique_classes), err_res, c="r", label='KDF')
+ax.plot(train_samples*len(unique_classes), err_res_rf, c="k", label='RF')
+
+ax.fill_between(train_samples*len(unique_classes), err_res_25_quantile, err_res_75_quantile, facecolor='r', alpha=.3)
+ax.fill_between(train_samples*len(unique_classes), err_res_25_quantile_rf, err_res_75_quantile_rf, facecolor='k', alpha=.3)
+
+ax.set_xscale('log')
+ax.set_xlabel('Sample size')
+ax.set_ylabel('Generalization Error')
+
+right_side = ax.spines["right"]
+right_side.set_visible(False)
+top_side = ax.spines["top"]
+top_side.set_visible(False)
+
+ax.legend(frameon=False)
+
+plt.savefig('plots/sim_res.pdf')
+
 # %%
