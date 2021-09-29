5a38eddbfd113ba511ff00b9849c9292634471a5
jdey4 <jdey4@jhmi.edu>
2021-09-29 15:59:56 -0500

    new exp code

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -12,17 +12,17 @@ from sklearn.metrics import cohen_kappa_score
 import os
 
 # %%
-task = openml.tasks.get_task(6)
+task = openml.tasks.get_task(12)
 X, y = task.get_X_and_y()
+#X = X[:,100:150]
 
 skf = StratifiedKFold(n_splits=5)
-train_index, test_index = list(skf.split(X, y))[1]
+train_index, test_index = list(skf.split(X, y))[0]
 
-model_kdf = kdf({'n_estimators':500})
+model_kdf = kdf(feature_sub=2, kwargs={'n_estimators':500})
 model_kdf.fit(X[train_index], y[train_index])
 
-model_rf = rf(n_estimators=500).fit(X[train_index], y[train_index])
 # %%
 test_acc = np.mean(model_kdf.predict(X[test_index])==y[test_index])
-test_acc2 = np.mean(model_rf.predict(X[test_index])==y[test_index])
+test_acc2 = np.mean(model_kdf.rf_model.predict(X[test_index])==y[test_index])
 # %%
--- kdg/kdf.py
+++ kdg/kdf.py
@@ -5,17 +5,19 @@ from sklearn.ensemble import RandomForestClassifier as rf
 import numpy as np
 from scipy.stats import multivariate_normal
 import warnings
-from sklearn.covariance import MinCovDet, fast_mcd, GraphicalLassoCV, LedoitWolf, EmpiricalCovariance, OAS, EllipticEnvelope
+from sklearn.covariance import LedoitWolf
 
 class kdf(KernelDensityGraph):
 
-    def __init__(self, kwargs={}):
+    def __init__(self, feature_sub=1, kwargs={}):
         super().__init__()
 
         self.polytope_means = {}
         self.polytope_cov = {}
         self.polytope_cardinality = {}
         self.polytope_mean_cov = {}
+        self.feature_used = {}
+        self.feature_sub = feature_sub
         self.kwargs = kwargs
         self.is_fitted = False
 
@@ -39,10 +41,24 @@ class kdf(KernelDensityGraph):
         self.labels = np.unique(y)
         self.rf_model = rf(**self.kwargs).fit(X, y)
         feature_dim = X.shape[1]
+        feature = list(range(feature_dim))
+        np.random.shuffle(feature)
+        feature_per_group = feature_dim//self.feature_sub
+        self.features = []
+        
+        for ii in range(self.feature_sub-1):
+            self.features.append(
+                feature[ii*feature_per_group:(ii+1)*feature_per_group]
+            )
+
+        self.features.append(
+            feature[(self.feature_sub-1)*feature_per_group:]
+        )
 
         for label in self.labels:
             self.polytope_means[label] = []
             self.polytope_cov[label] = []
+            self.feature_used[label] = []
 
             X_ = X[np.where(y==label)[0]]
             predicted_leaf_ids_across_trees = np.array(
@@ -65,30 +81,46 @@ class kdf(KernelDensityGraph):
                 if len(idx) == 1:
                     continue
                 
+
                 scales = matched_samples[idx]/np.max(matched_samples[idx])
-                X_tmp = X_[idx].copy()
-                location_ = np.average(X_tmp, axis=0, weights=scales)
-                X_tmp -= location_
-                
-                sqrt_scales = np.sqrt(scales).reshape(-1,1) @ np.ones(feature_dim).reshape(1,-1)
-                X_tmp *= sqrt_scales
 
-                covariance_model = LedoitWolf(assume_centered=True)
-                covariance_model.fit(X_tmp)
+                for sub_group in range(self.feature_sub):
+                    X_tmp = X_[idx].copy()
+                    X_tmp = X_tmp[:,self.features[sub_group]].copy()
+                    #print(X_tmp.shape)
+                    location_ = np.average(X_tmp, axis=0, weights=scales)
+                    X_tmp -= location_
+                    
+                    sqrt_scales = np.sqrt(scales).reshape(-1,1) @ np.ones(len(self.features[sub_group])).reshape(1,-1)
+                    X_tmp *= sqrt_scales
+                    #X_tmp[:,-10:-1] *= 1.2
+                    
+                    covariance_model = LedoitWolf(assume_centered=True)
+                    covariance_model.fit(X_tmp)
 
-                self.polytope_means[label].append(
-                    location_
-                )
-                self.polytope_cov[label].append(
-                    covariance_model.covariance_*len(idx)/sum(scales)
-                )
-        
+                    self.polytope_means[label].append(
+                        location_
+                    )
+                    self.polytope_cov[label].append(
+                        covariance_model.covariance_*len(idx)/sum(scales)
+                    )
+                    self.feature_used[label].append(
+                        self.features[sub_group]
+                    )
+
+            '''self.polytope_mean_cov[label] = np.average(
+                    self.polytope_cov[label], 
+                    axis= 0,
+                    weights = self.polytope_cardinality[label]
+                    )'''
+
         self.is_fitted = True
         
             
     def _compute_pdf(self, X, label, polytope_idx):
         polytope_mean = self.polytope_means[label][polytope_idx]
         polytope_cov = self.polytope_cov[label][polytope_idx]
+        #polytope_cov = self.polytope_mean_cov[label]
 
         var = multivariate_normal(
             mean=polytope_mean, 
@@ -116,7 +148,7 @@ class kdf(KernelDensityGraph):
         
         for ii,label in enumerate(self.labels):
             for polytope_idx,_ in enumerate(self.polytope_means[label]):
-                likelihoods[:,ii] += np.nan_to_num(self._compute_pdf(X, label, polytope_idx))
+                likelihoods[:,ii] += np.nan_to_num(self._compute_pdf(X[:,self.feature_used[label][polytope_idx]], label, polytope_idx))
 
         proba = (likelihoods.T/(np.sum(likelihoods,axis=1)+1e-100)).T
         return proba
