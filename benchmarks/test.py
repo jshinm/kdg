cfc88940dca336ebabb4d7b44457727df8dae33f
jdey4 <jdey4@jhmi.edu>
2021-09-29 16:01:12 -0500

    new exp code

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -10,19 +10,84 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier as rf
 from sklearn.metrics import cohen_kappa_score
 import os
-
+import math
 # %%
+mc_reps = 10
+n_estimators = 500
+
 task = openml.tasks.get_task(12)
 X, y = task.get_X_and_y()
-#X = X[:,100:150]
+total_sample = X.shape[0]
+unique_classes, counts = np.unique(y, return_counts=True)
 
-skf = StratifiedKFold(n_splits=5)
-train_index, test_index = list(skf.split(X, y))[0]
+test_sample = 100
+train_samples = [10, 50, 100]
+indx = []
 
-model_kdf = kdf(feature_sub=2, kwargs={'n_estimators':500})
-model_kdf.fit(X[train_index], y[train_index])
+for label in unique_classes:
+    indx.append(
+        np.where(
+            y==label
+        )[0]
+    )
+#%%
+err_res = []
+err_res_25_quantile = []
+err_res_75_quantile = []
 
-# %%
-test_acc = np.mean(model_kdf.predict(X[test_index])==y[test_index])
-test_acc2 = np.mean(model_kdf.rf_model.predict(X[test_index])==y[test_index])
+err_res_rf = []
+err_res_25_quantile_rf = []
+err_res_75_quantile_rf = []
+
+for train_sample in train_samples:
+    err = []
+    err_ = []
+    for _ in range(mc_reps):
+        indx_to_take_train = []
+        indx_to_take_test = []
+
+        for ii, _ in enumerate(unique_classes):
+            np.random.shuffle(indx[ii])
+            indx_to_take_train.extend(
+                list(
+                        indx[ii][:train_sample]
+                )
+            )
+            indx_to_take_test.extend(
+                list(
+                        indx[ii][-test_sample:counts[ii]]
+                )
+            )
+        model_kdf = kdf(feature_sub=2, kwargs={'n_estimators':500})
+        model_kdf.fit(X[indx_to_take_train], y[indx_to_take_train])
+        err.append(
+            np.mean(
+                model_kdf.predict(X[indx_to_take_test])==y[indx_to_take_test]
+            )
+        )
+        err_.append(
+            np.mean(
+                model_kdf.rf_model.predict(X[indx_to_take_test])==y[indx_to_take_test]
+            )
+        )
+
+    err_res.append(
+        np.median(err)
+    )
+    err_res_25_quantile.append(
+        np.quantile(err,[.25])[0]
+    )
+    err_res_75_quantile.append(
+        np.quantile(err,[.75])[0]
+    )
+
+    err_res_rf.append(
+        np.median(err_)
+    )
+    err_res_25_quantile_rf.append(
+        np.quantile(err_,[.25])[0]
+    )
+    err_res_75_quantile_rf.append(
+        np.quantile(err_,[.75])[0]
+    )
 # %%
