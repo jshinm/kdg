72cb37b94c3ea124a5cc5caa9af6ab37c39fd091
Haoyin Xu <haoyinxu@gmail.com>
2022-01-03 22:50:07 -0500

    ENH add rkdf code for 4 model fig

--- benchmarks/high_dim_sim.py
+++ benchmarks/high_dim_sim.py
@@ -1,10 +1,10 @@
-#%%
 import numpy as np
-from kdg import kdf
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kdg import kdf, rkdf
 from kdg.utils import gaussian_sparse_parity, trunk_sim
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier as rf
-#%%
+
 p = 20
 p_star = 3
 '''sample_size = np.logspace(
@@ -22,11 +22,12 @@ n_estimators = 500
 df = pd.DataFrame()
 reps_list = []
 accuracy_kdf = []
-accuracy_kdf_ = []
+# accuracy_kdf_ = []
 accuracy_rf = []
-accuracy_rf_ = []
+# accuracy_rf_ = []
 sample_list = []
-# %%
+accuracy_rkdf = []
+accuracy_et = []
 for sample in sample_size:
     print('Doing sample %d'%sample)
     for ii in range(reps):
@@ -42,10 +43,7 @@ for sample in sample_size:
         )
 
         #train kdf
-        model_kdf = kdf(
-            n_estimators=n_estimators,
-            max_features=1
-        )
+        model_kdf = kdf(n_estimators=n_estimators)
 
         model_kdf.fit(X, y)
         accuracy_kdf.append(
@@ -58,25 +56,40 @@ for sample in sample_size:
                 model_kdf.rf_model.predict(X_test) == y_test
             )
         )
+
+        # train rkdf
+        model_rkdf = rkdf(n_estimators=n_estimators, bootstrap=True, max_features=1)
+
+        model_rkdf.fit(X, y)
+        accuracy_rkdf.append(
+            np.mean(
+                model_rkdf.predict(X_test) == y_test
+            )
+        )
+        accuracy_et.append(
+            np.mean(
+                model_rkdf.et_model.predict(X_test) == y_test
+            )
+        )
+
         reps_list.append(ii)
         sample_list.append(sample)
-        print(accuracy_kdf)
+        # print(accuracy_kdf)
         #train feature selected kdf
 
 
 df['accuracy kdf'] = accuracy_kdf
+df['accuracy rkdf'] = accuracy_rkdf
 #df['feature selected kdf'] = accuracy_kdf_
 df['accuracy rf'] = accuracy_rf
+df['accuracy et'] = accuracy_et
 #df['feature selected rf'] = accuracy_rf_
 df['reps'] = reps_list
 df['sample'] = sample_list
 
 df.to_csv('high_dim_res_kdf_gaussian_random_splits.csv')
 # %% plot the result
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 filename1 = 'high_dim_res_kdf_gaussian_random_splits.csv'
 
@@ -88,26 +101,33 @@ err_rf_med = []
 err_rf_25_quantile = []
 err_rf_75_quantile = []
 
-err_rf_med_ = []
-err_rf_25_quantile_ = []
-err_rf_75_quantile_ = []
+err_et_med = []
+err_et_25_quantile = []
+err_et_75_quantile = []
+
+# err_rf_med_ = []
+# err_rf_25_quantile_ = []
+# err_rf_75_quantile_ = []
 
 err_kdf_med = []
 err_kdf_25_quantile = []
 err_kdf_75_quantile = []
 
-err_kdf_med_ = []
-err_kdf_25_quantile_ = []
-err_kdf_75_quantile_ = []
+err_rkdf_med = []
+err_rkdf_25_quantile = []
+err_rkdf_75_quantile = []
+
+# err_kdf_med_ = []
+# err_kdf_25_quantile_ = []
+# err_kdf_75_quantile_ = []
 #clr = ["#e41a1c", "#f781bf", "#306998"]
 #c = sns.color_palette(clr, n_colors=3)
 
 
 for sample in sample_size:
-    err_rf = 1 - df['accuracy rf'][df['sample']==sample]
     #err_rf_ = 1 - df['feature selected rf'][df['sample']==sample]
-    err_kdf = 1 - df['accuracy kdf'][df['sample']==sample]
     #err_kdf_ = 1 - df['feature selected kdf'][df['sample']==sample]
+    err_rf = 1 - df['accuracy rf'][df['sample']==sample]
 
     err_rf_med.append(np.median(err_rf))
     err_rf_25_quantile.append(
@@ -117,6 +137,16 @@ for sample in sample_size:
         np.quantile(err_rf,[.75])[0]
     )
 
+    err_et = 1 - df['accuracy et'][df['sample']==sample]
+
+    err_et_med.append(np.median(err_et))
+    err_et_25_quantile.append(
+            np.quantile(err_et,[.25])[0]
+        )
+    err_et_75_quantile.append(
+        np.quantile(err_et,[.75])[0]
+    )
+
     #err_rf_med_.append(np.median(err_rf_))
     #err_rf_25_quantile_.append(
     #        np.quantile(err_rf_,[.25])[0]
@@ -124,6 +154,7 @@ for sample in sample_size:
     #err_rf_75_quantile_.append(
     #    np.quantile(err_rf_,[.75])[0]
     #)
+    err_kdf = 1 - df['accuracy kdf'][df['sample']==sample]
 
     err_kdf_med.append(np.median(err_kdf))
     err_kdf_25_quantile.append(
@@ -133,6 +164,15 @@ for sample in sample_size:
         np.quantile(err_kdf,[.75])[0]
     )
 
+    err_rkdf = 1 - df['accuracy rkdf'][df['sample']==sample]
+
+    err_rkdf_med.append(np.median(err_rkdf))
+    err_rkdf_25_quantile.append(
+            np.quantile(err_rkdf,[.25])[0]
+        )
+    err_rkdf_75_quantile.append(
+        np.quantile(err_rkdf,[.75])[0]
+    )
     #err_kdf_med_.append(np.median(err_kdf_))
     #err_kdf_25_quantile_.append(
     #        np.quantile(err_kdf_,[.25])[0]
@@ -144,14 +184,20 @@ for sample in sample_size:
 sns.set_context('talk')
 fig, ax = plt.subplots(1,1, figsize=(8,8))
 
-ax.plot(sample_size, err_rf_med, c="k", label='ET')
+ax.plot(sample_size, err_rf_med, c="k", label='RF')
 ax.fill_between(sample_size, err_rf_25_quantile, err_rf_75_quantile, facecolor='k', alpha=.3)
+
+ax.plot(sample_size, err_et_med, c="b", label='ET')
+ax.fill_between(sample_size, err_et_25_quantile, err_et_75_quantile, facecolor='b', alpha=.3)
 
 #ax.plot(sample_size, err_rf_med_, c="g", label='RF (feature selected)')
 #ax.fill_between(sample_size, err_rf_25_quantile_, err_rf_75_quantile_, facecolor='g', alpha=.3)
 
-ax.plot(sample_size, err_kdf_med, c="r", label='KDF-ET')
+ax.plot(sample_size, err_kdf_med, c="r", label='KDF')
 ax.fill_between(sample_size, err_kdf_25_quantile, err_kdf_75_quantile, facecolor='r', alpha=.3)
+
+ax.plot(sample_size, err_rkdf_med, c="g", label='RKDF')
+ax.fill_between(sample_size, err_rkdf_25_quantile, err_rkdf_75_quantile, facecolor='g', alpha=.3)
 
 #ax.plot(sample_size, err_kdf_med_, c="b", label='KDF (feteaure selected)')
 #ax.fill_between(sample_size, err_kdf_25_quantile_, err_kdf_75_quantile_, facecolor='b', alpha=.3)
@@ -167,5 +213,3 @@ ax.set_ylabel('error')
 ax.legend(frameon=False)
 
 plt.savefig('plots/high_dim_gaussian_random_splits.pdf')
-
-# %%
--- kdg/kdf.py
+++ kdg/kdf.py
@@ -1,7 +1,8 @@
 from .base import KernelDensityGraph
 from sklearn.mixture import GaussianMixture
 from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
-from sklearn.ensemble import ExtraTreesClassifier as rf
+from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.ensemble import ExtraTreesClassifier as et
 import numpy as np
 from scipy.stats import multivariate_normal
 import warnings
@@ -49,6 +50,148 @@ class kdf(KernelDensityGraph):
             X_ = X[np.where(y==label)[0]]
             predicted_leaf_ids_across_trees = np.array(
                 [tree.apply(X_) for tree in self.rf_model.estimators_]
+                ).T
+            '''_, polytope_idx = np.unique(
+                predicted_leaf_ids_across_trees, return_inverse=True, axis=0
+            )
+            total_polytopes_this_label = np.max(polytope_idx)+1'''
+            total_samples_this_label = X_.shape[0]
+
+            for polytope in range(total_samples_this_label):
+                matched_samples = np.sum(
+                    predicted_leaf_ids_across_trees == predicted_leaf_ids_across_trees[polytope],
+                    axis=1
+                )
+                idx = np.where(
+                    matched_samples>0
+                )[0]
+
+                if len(idx) == 1:
+                    continue
+
+                scales = matched_samples[idx]/np.max(matched_samples[idx])
+                X_tmp = X_[idx].copy()
+                location_ = np.average(X_tmp, axis=0, weights=scales)
+                X_tmp -= location_
+
+                sqrt_scales = np.sqrt(scales).reshape(-1,1) @ np.ones(feature_dim).reshape(1,-1)
+                X_tmp *= sqrt_scales
+
+                covariance_model = LedoitWolf(assume_centered=True)
+                covariance_model.fit(X_tmp)
+
+                self.polytope_means[label].append(
+                    location_
+                )
+                self.polytope_cov[label].append(
+                    covariance_model.covariance_*len(idx)/sum(scales)
+                )
+
+            ## calculate bias for each label
+            likelihoods = np.zeros(
+                (np.size(X_,0)),
+                dtype=float
+            )
+            for polytope_idx,_ in enumerate(self.polytope_means[label]):
+                likelihoods += np.nan_to_num(self._compute_pdf(X_, label, polytope_idx))
+
+            likelihoods /= total_samples_this_label
+            self.bias[label] = np.min(likelihoods)/(self.k*total_samples_this_label)
+
+        self.is_fitted = True
+
+
+    def _compute_pdf(self, X, label, polytope_idx):
+        polytope_mean = self.polytope_means[label][polytope_idx]
+        polytope_cov = self.polytope_cov[label][polytope_idx]
+
+        var = multivariate_normal(
+            mean=polytope_mean,
+            cov=polytope_cov,
+            allow_singular=True
+            )
+
+        likelihood = var.pdf(X)
+        return likelihood
+
+    def predict_proba(self, X):
+        r"""
+        Calculate posteriors using the kernel density forest.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        """
+        X = check_array(X)
+
+        likelihoods = np.zeros(
+            (np.size(X,0), len(self.labels)),
+            dtype=float
+        )
+
+        for ii,label in enumerate(self.labels):
+            total_polytopes = len(self.polytope_means[label])
+            for polytope_idx,_ in enumerate(self.polytope_means[label]):
+                likelihoods[:,ii] += np.nan_to_num(self._compute_pdf(X, label, polytope_idx))
+
+            likelihoods[:,ii] = likelihoods[:,ii]/total_polytopes
+            likelihoods[:,ii] += self.bias[label]
+
+        proba = (likelihoods.T/np.sum(likelihoods,axis=1)).T
+        return proba
+
+    def predict(self, X):
+        r"""
+        Perform inference using the kernel density forest.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        """
+        return np.argmax(self.predict_proba(X), axis = 1)
+
+class rkdf(KernelDensityGraph):
+
+    def __init__(self, k = 1, **kwargs):
+        super().__init__()
+
+        self.polytope_means = {}
+        self.polytope_cov = {}
+        self.polytope_cardinality = {}
+        self.polytope_mean_cov = {}
+        self.bias = {}
+        self.kwargs = kwargs
+        self.k = k
+        self.is_fitted = False
+
+    def fit(self, X, y):
+        r"""
+        Fits the kernel density forest.
+        Parameters
+        ----------
+        X : ndarray
+            Input data matrix.
+        y : ndarray
+            Output (i.e. response) data matrix.
+        """
+        if self.is_fitted:
+            raise ValueError(
+                "Model already fitted!"
+            )
+            return
+
+        X, y = check_X_y(X, y)
+        self.labels = np.unique(y)
+        self.et_model = et(**self.kwargs).fit(X, y)
+        feature_dim = X.shape[1]
+
+        for label in self.labels:
+            self.polytope_means[label] = []
+            self.polytope_cov[label] = []
+
+            X_ = X[np.where(y==label)[0]]
+            predicted_leaf_ids_across_trees = np.array(
+                [tree.apply(X_) for tree in self.et_model.estimators_]
                 ).T
             '''_, polytope_idx = np.unique(
                 predicted_leaf_ids_across_trees, return_inverse=True, axis=0
